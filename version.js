678f91de8f2393283c757d082dbb444f92299ca3
koteitan <ukaibutton@mac.com>
2019-12-25 00:06:47 -0500

    draw grid

--- version.js
+++ version.js
@@ -1,9 +1,1 @@
 document.getElementById("versiondisplay").innerHTML = "0.1";
-document.getElementById("changelog").innerHTML = 
-"<ul>"+
-"<li>2019/12/24 start implementation.</li>"+
-"</ul>";
-document.getElementById("knownbugs").innerHTML = 
-"<ul>"+
-"<li>under construction</li>"+
-"</ul>";
